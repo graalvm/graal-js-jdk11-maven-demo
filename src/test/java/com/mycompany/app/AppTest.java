aad11783785caa70639c8048dd5b4ac0f235a86a
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-10-11 11:46:11 +0200

    Simplify the test by using junit 4.12.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,37 +1,16 @@
 package com.mycompany.app;
 
-import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-       return new TestSuite( AppTest.class );
-    }
-
+public class AppTest {
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
     	Assert.assertTrue(App.get42().isNumber());
